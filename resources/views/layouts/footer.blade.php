87d3a6e2d17788bd7bea61682d9cb42a8719f464
rinal <rinalditoryanari@gmail.com>
2024-10-09 11:43:11 +0700

    duuh

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,6 @@
+{{--<div class="footer-wrap pd-20 mb-20 card-box">--}}
+{{--    Absensi - Designed & Developed By <a href="https://github.com/ambonxnobody" target="_blank">ahmad hilmy</a> dan <a href="https://github.com/rinalditoryanari" target="_blank">Rinaldito AR</a>--}}
+{{--</div>--}}
 <div class="footer-wrap pd-20 mb-20 card-box">
-    Absensi - Designed & Developed By <a href="https://github.com/ambonxnobody" target="_blank">ahmad hilmy</a> dan <a href="https://github.com/rinalditoryanari" target="_blank">Rinaldito AR</a>
+    Absensi - Designed & Developed By <a href="https://github.com/rinalditoryanari" target="_blank">Rinaldito AR</a>
 </div>

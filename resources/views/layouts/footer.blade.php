789c43765fe409e59392d0ce1619d540a13d3458
rinal <rinalditoryanari@gmail.com>
2024-10-09 08:35:11 +0700

    add my github

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,3 @@
 <div class="footer-wrap pd-20 mb-20 card-box">
-    Absensi - Designed & Developed By <a href="https://github.com/ambonxnobody" target="_blank">ahmad hilmy</a>
+    Absensi - Designed & Developed By <a href="https://github.com/ambonxnobody" target="_blank">ahmad hilmy</a> dan <a href="https://github.com/rinalditoryanari" target="_blank">Rinaldito AR</a>
 </div>
